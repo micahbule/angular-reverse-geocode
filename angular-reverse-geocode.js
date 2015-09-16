dac6b749e18b551ea25e0fc645c04267f40a9dd5
Mico <mico@cynder.io>
2015-09-16 23:30:20 +0800

    Used scope instead of attributes to avoid curly-brace expression bindings in the DOM. Also used scope so you can watch the scope variables and update the DOM after changes (ex. Promise-based lat-lng coordinates).

--- angular-reverse-geocode.js
+++ angular-reverse-geocode.js
@@ -9,20 +9,27 @@
         return {
             restrict: 'E',
             template: '<div></div>',
-            link: function (scope, element, attrs) {
-                var geocoder = new google.maps.Geocoder();
-                var latlng = new google.maps.LatLng(attrs.lat, attrs.lng);
-                geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        if (results[1]) {
-                            element.text(results[1].formatted_address);
+            scope: {
+                lat: '=',
+                lng: '='
+            },
+            link: function (scope, element) {
+                scope.$watchGroup(['lat', 'lng'], function () {
+                    var geocoder = new google.maps.Geocoder();
+                    var latlng = new google.maps.LatLng(scope.lat, scope.lng);
+                    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            if (results[1]) {
+                                element.text(results[1].formatted_address);
+                            } else {
+                                element.text('Location not found');
+                            }
                         } else {
-                            element.text('Location not found');
+                            element.text('Geocoder failed due to: ' + status);
                         }
-                    } else {
-                        element.text('Geocoder failed due to: ' + status);
-                    }
+                    });
                 });
+
             },
             replace: true
         }
